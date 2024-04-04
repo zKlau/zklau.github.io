7f540b05b78b027dcd0bad689ecefbc794e6efb6
zKlau <37957524+zKlau@users.noreply.github.com>
2024-04-04 12:39:59 +0000

    removed verification

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ app.use(express.static(__dirname + '/public'))
 
 // Define Port
 const port = 3000
-app.get('/.well-known/discord', (req,res) => {
-	res.send("dh=16a691e528e42c85ab5d0073f1d87db29fc3dcf7")
-});
+
 // Listen on defined port
 app.listen(port, () => {
     console.log(`Web server started!`)
